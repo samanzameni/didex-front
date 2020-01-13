cc6542b95e9f74e01c757213d90288225a29dc60
Azad Kavian <azad.kavian@gmail.com>
2020-01-13 18:46:33 +0330

    added radio button

--- src/app/widget/components/radio-button/radio-button.component.ts
+++ src/app/widget/components/radio-button/radio-button.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DataEntry } from '@widget/templates';
 
 @Component({
-  selector: 'app-radio-button',
+  selector: 'radio-button',
   templateUrl: './radio-button.component.html',
-  styleUrls: ['./radio-button.component.scss']
+  styleUrls: ['./radio-button.component.scss'],
 })
-export class RadioButtonComponent implements OnInit {
+export class RadioButtonComponent extends DataEntry<string> implements OnInit {
+  @Input() actualValue: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    super();
   }
 
+  ngOnInit() {
+    super.ngOnInit();
+  }
+
+  get isValid(): boolean {
+    return true;
+  }
+
+  onPush($event): void {
+    if ($event.target.checked) {
+      this.data = $event.target.value;
+      this.valueChange.emit(this.value);
+    } else {
+      this.data = undefined;
+    }
+  }
 }

366b807c29f09a97bcc6e2593bdefe823f4a7ab2
Azad Kavian <azad.kavian@gmail.com>
2020-02-22 15:22:13 +0330

    wtf

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RESTModule } from './modules/ddx-rest.module';
 import { DATAModule } from './modules/ddx-data.module';
-import { StorageService, AuthService } from './services';
+import { StorageService, AuthService, SignalRService } from './services';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [RESTModule, HttpClientModule, DATAModule],
-  providers: [StorageService, AuthService],
+  providers: [StorageService, AuthService, SignalRService],
 })
 export class CoreModule {}

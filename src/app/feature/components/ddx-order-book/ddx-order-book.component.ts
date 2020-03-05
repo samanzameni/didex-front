6b3338754b07feeb5355d0af3db469532c7adad0
Azad Kavian <azad.kavian@gmail.com>
2020-03-05 15:00:31 +0330

    finished orderbook styles and data n stuff

--- src/app/feature/components/ddx-order-book/ddx-order-book.component.ts
+++ src/app/feature/components/ddx-order-book/ddx-order-book.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {
-  TradeSymbol,
-  TradeOrder,
-  OrderBookResponse,
-  OrderBookRecord,
-} from '@core/models';
+import { TradeSymbol, OrderBookResponse, OrderBookRecord } from '@core/models';
+
+import { Decimal } from 'decimal.js';
 
 @Component({
   selector: 'ddx-order-book',
@@ -22,6 +19,38 @@ export class OrderBookComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  get bidsSumArray(): Decimal[] {
+    const result: Decimal[] = [];
+    let sum: Decimal = new Decimal(0);
+    for (const bidRow of this.bidsTableData) {
+      sum = sum.add(bidRow.volume);
+      result.push(sum);
+    }
+    return result;
+  }
+
+  get bidsSum(): Decimal {
+    return this.bidsSumArray && this.bidsSumArray.length > 0
+      ? this.bidsSumArray[this.bidsSumArray.length - 1]
+      : new Decimal(0);
+  }
+
+  get asksSumArray(): Decimal[] {
+    const result: Decimal[] = [];
+    let sum: Decimal = new Decimal(0);
+    for (const askRow of this.asksTableData) {
+      sum = sum.add(askRow.volume);
+      result.push(sum);
+    }
+    return result;
+  }
+
+  get asksSum(): Decimal {
+    return this.asksSumArray && this.asksSumArray.length > 0
+      ? this.asksSumArray[this.asksSumArray.length - 1]
+      : new Decimal(0);
+  }
+
   get bidsTableData(): OrderBookRecord[] {
     return this.orderBookData
       ? this.orderBookData.bid.sort(this.bidSorter)
@@ -32,6 +61,28 @@ export class OrderBookComponent implements OnInit {
     return this.orderBookData
       ? this.orderBookData.ask.sort(this.askSorter)
       : [];
+  }
+
+  get asksTotal(): Decimal {
+    let summation: Decimal = new Decimal(0);
+
+    this.asksTableData.forEach(askRecord => {
+      summation = summation.add(askRecord.volume);
+    });
+
+    return summation;
+  }
+
+  get bidsTotal(): Decimal {
+    let summation: Decimal = new Decimal(0);
+
+    this.bidsTableData.forEach(bidRecord => {
+      summation = summation.add(
+        new Decimal(bidRecord.volume).mul(bidRecord.price)
+      );
+    });
+
+    return summation;
   }
 
   private bidSorter(a: OrderBookRecord, b: OrderBookRecord): number {

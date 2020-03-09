0ae1bcc8d5acd8dea11c4bc53c901017661fafca
Azad Kavian <azad.kavian@gmail.com>
2020-03-10 00:25:41 +0330

    added balance updater interval

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RESTModule } from './modules/ddx-rest.module';
 import { DATAModule } from './modules/ddx-data.module';
-import {
-  StorageService,
-  AuthService,
-  SignalRService,
-  TraderService,
-} from './services';
+import { INTERVALModule } from './modules/ddx-interval.module';
+import { StorageService, AuthService, TraderService } from './services';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
-  imports: [HttpClientModule, DATAModule, RESTModule],
+  imports: [HttpClientModule, DATAModule, RESTModule, INTERVALModule],
   providers: [StorageService, AuthService, TraderService],
 })
 export class CoreModule {}

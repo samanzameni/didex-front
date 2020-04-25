8d6c3a0aa81f97f7768750dda0ab8994b05ecd98
Azad Kavian <azad.kavian@gmail.com>
2020-04-25 21:27:18 +0430

    added tooltips for orderbook

--- src/app/feature/modules/main.module.ts
+++ src/app/feature/modules/main.module.ts
@@ -25,6 +25,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const routes: Routes = [
   {
@@ -79,6 +81,8 @@ const routes: Routes = [
     MatButtonModule,
     MatDividerModule,
     MatSelectModule,
+    MatTableModule,
+    MatTooltipModule,
   ],
   exports: [],
   providers: [],

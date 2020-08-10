ed0762313b12131a5827a53a58e72e2ea50501f7
Azad Kavian <azad.kavian@gmail.com>
2020-08-10 19:02:54 +0430

    change ch locale files to zh

---
+++ src/locale/zh/index.ts
@@ -0,0 +1,6 @@
+export { default as funds } from './funds.zh.json';
+export { default as homepage } from './homepage.zh.json';
+export { default as kyc } from './kyc.zh.json';
+export { default as reports } from './reports.zh.json';
+export { default as settings } from './settings.zh.json';
+export { default as navbar } from './navbar.zh.json';

d34ea2465eddd0813ad47d193016a584f4c9663d
saman zameni <saman.zameni@gmail.com>
2020-09-08 16:40:18 +0430

    adding 404 page.

---
+++ src/app/feature/components/ddx-wrong-page/ddx-wrong-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ddx-wrong-page',
+  templateUrl: './ddx-wrong-page.component.html',
+  styleUrls: ['./ddx-wrong-page.component.scss']
+})
+export class DdxWrongPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WidgetModule } from '@widget/widget.module';
 import { MainModule } from '@feature/modules/main.module';
+import { DdxWrongPageComponent } from './components/ddx-wrong-page/ddx-wrong-page.component';
 
 @NgModule({
   imports: [CommonModule, WidgetModule, MainModule],
-  declarations: [],
+  declarations: [DdxWrongPageComponent],
 })
 export class FeatureModule {}
